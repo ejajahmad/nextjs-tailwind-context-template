b8fbfaf108e589fec0f5750e283d3c2fa64e2329
Ejaj Ahmad <ajasaifi2018@gmail.com>
2022-10-09 17:22:49 +0530

    Init

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import { StateContext } from "../context/StateContext";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <StateContext>
+      <Component {...pageProps} />
+    </StateContext>
+  );
 }
 
-export default MyApp
+export default MyApp;
